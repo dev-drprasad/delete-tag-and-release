34f3ac2165dbb31c34981863244884e23ff5bfd5
Thad Guidry <thadguidry@gmail.com>
2022-12-13 19:47:01 +0800

    Revert "Add option to delete draft releases + README fix"

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ if (!process.env.INPUT_TAG_NAME) {
 const tagName = process.env.INPUT_TAG_NAME;
 
 const shouldDeleteRelease = process.env.INPUT_DELETE_RELEASE === "true";
-const shouldDeleteDraftRelease = process.env.INPUT_DELETE_DRAFT_RELEASE === "true";
 
 const commonOpts = {
   host: "api.github.com",
@@ -75,7 +74,7 @@ async function deleteReleases() {
       method: "GET",
     });
     releaseIds = (data || [])
-      .filter(({ tag_name, draft }) => tag_name === tagName && shouldDeleteDraftRelease ? true : (draft === false))
+      .filter(({ tag_name, draft }) => tag_name === tagName && draft === false)
       .map(({ id }) => id);
   } catch (error) {
     console.error(`🌶  failed to get list of releases <- ${error.message}`);
